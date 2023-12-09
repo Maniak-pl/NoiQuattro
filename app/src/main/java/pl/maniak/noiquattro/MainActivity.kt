94b62d3c89d23188b90a9b029e97fd5d91371f92
Piotr Liszka <piotr.liszka.pl@gmail.com>
2023-12-09 11:16:10 +0100

    16. Navigation

--- app/src/main/java/pl/maniak/noiquattro/MainActivity.kt
+++ app/src/main/java/pl/maniak/noiquattro/MainActivity.kt
@@ -3,12 +3,14 @@ package pl.maniak.noiquattro
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.ui.Modifier
-import pl.maniak.noiquattro.data.samples.sampleMapData
-import pl.maniak.noiquattro.ui.screens.MapScreen
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import pl.maniak.noiquattro.ui.screens.LoginScreen
+import pl.maniak.noiquattro.ui.screens.StartScreen
 import pl.maniak.noiquattro.ui.theme.NoiquattroTheme
 
 class MainActivity : ComponentActivity() {
@@ -16,14 +18,47 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             NoiquattroTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    MapScreen(data = sampleMapData)
+                Surface {
+                    val navController = rememberNavController()
+                    NavigationComponent(navController = navController)
                 }
             }
         }
     }
 }
 
+@Composable
+fun NavigationComponent(navController: NavHostController) {
+    NavHost(navController = navController, startDestination = "start") {
+        composable(route = "start") {
+            StartScreen(onStartClick = { navController.navigate("login") })
+        }
+        composable(route = "login") {
+            LoginScreen(
+                onClickLogin = { str1, str2 -> navController.navigate("home") },
+                onClickGoogle = {}
+            )
+        }
+        composable(route = "home") {
+
+        }
+        composable(route = "item_detail") {
+
+        }
+        composable(route = "profile") {
+
+        }
+        composable(route = "shopping_bag") {
+
+        }
+        composable(route = "payment") {
+
+        }
+        composable(route = "order_history") {
+
+        }
+        composable(route = "map") {
+
+        }
+    }
+}
